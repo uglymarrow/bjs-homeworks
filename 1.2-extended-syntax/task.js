bfbdb6ba67b37c8de1d5f7d5d8c5f3a1afe5a4f4
Ksenia Yaroshenko <ksus2000@yandex.ru>
2020-07-15 17:49:34 +0300

    Выполнение задания 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,45 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let coefA = a;
+    let coefB = b;
+    let coefC = c;
+    let x = [];
+    let D = coefB ** 2 - 4 * coefA * coefC;
+
+    if (D > 0) {
+        x[0] = (-coefB + Math.sqrt(D)) / (2 * coefA);
+        x[1] = (-coefB - Math.sqrt(D)) / (2 * coefA);
+    } else if (D === 0) {
+        x[0] = -coefB / 2 * coefA;
+    } 
+
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let curMarks = marks;
+    let averageMark, num = 0, sum = 0;
+    let curMarksLen = curMarks.length;
+
+    if (curMarksLen > 5) {
+        curMarksLen = 5;
+    }
+
+    if (curMarksLen === 0) {
+        return averageMark = 0;
+    } else {
+        for (let i = 0; i < curMarksLen; i ++) {
+            num ++;
+            sum += curMarks[i];
+        }
+
+        return averageMark = sum / num;
+    }
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
-}
+    let curName = name;
+    let curDateOfBirthday = dateOfBirthday;
+    let curDate = new Date;
+    let age = (curDate - curDateOfBirthday) / (1000 * 3600 * 24 * 365);
+    return result = (age >= 18) ? `Не желаете ли олд-фэшн, ${curName}?` : `Сожалею, ${curName}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
+} 
