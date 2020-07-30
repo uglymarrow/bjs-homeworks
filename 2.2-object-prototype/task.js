f18bc6998097155f88197c3526d5007753cb6ca6
Ksenia Yaroshenko <ksus2000@yandex.ru>
2020-07-30 11:12:43 +0300

    Выполнение задания 2.2 - объекты и прототипы

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,29 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+    const inputString = this.replace(/\s+/g, '').toLowerCase();
+
+    for (let i = 0; i < inputString.length; i++) {
+        if (inputString[i] != inputString[inputString.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sumOfMarks = 0;
+
+    for (mark of marks) {
+        sumOfMarks += mark;
+    }
+    const averageMark = (marks.length == 0) ? 0 : Math.round(sumOfMarks / marks.length);
+
+    return averageMark; 
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = + Date.now(); 
+    const unixStampBirth = + new Date(birthday);
+    const age =  (now - unixStampBirth) / (1000 * 3600 * 24 * 30 * 12);
+    const verdict = (age >= 18) ? true : false;
+    return verdict;
 }
